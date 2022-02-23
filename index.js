a8e7696d2df2481514e0977380561514a3692c34
Michael Tran <mltrunner@gmail.com>
2022-02-23 09:16:18 -0800

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const manhattanBlock = 264;
+const scuberHeadquarters = 42;
+
+
+function distanceFromHqInBlocks(location) {
+	return Math.abs(scuberHeadquarters - location);
+}
+
+function distanceFromHqInFeet(location) {
+	return distanceFromHqInBlocks(location) * manhattanBlock;
+}
+
+function distanceTravelledInFeet(loc1, loc2) {
+	return Math.abs((loc2 - loc1) * manhattanBlock);
+}
+
+function calculatesFarePrice(start, destination) {
+	const travelDistance = distanceTravelledInFeet(start, destination);
+	const flatFare = 25;	// 2000ft - 2500ft flate fare
+	const perFoot = .02;	// 400 - 2000 per foot cost
+
+	if (travelDistance > 2500) {
+		return 'cannot travel that far';
+	} else if (travelDistance > 2000) {
+		return 25;
+	} else if (travelDistance > 400) {
+		return (travelDistance - 400) * .02;
+	} else {	// first 400 ft free ride
+		return 0;
+	}
+}
